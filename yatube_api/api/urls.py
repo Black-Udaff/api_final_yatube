6bb769cdfde2be8ce15fb4a97cd187014cfc46ac
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-12-21 16:15:39 +0300

    123

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('v1/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('v1/', include('djoser.urls.jwt')),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,24 @@
 # TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+from posts.models import Post, Group, Follow
+from .serializers import (
+    PostSerializer,
+    GroupSerializer,
+    CommentSerializer,
+    FollowSerializer,
+)
+
+
+class FolowViewSet(viewsets.ModelViewSet):
+    serializer_class = FollowSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return user.follower.all()
+
+    def perform_create(self, serializer):
+        if serializer.validated_data.get('following') == self.request.user:
+            raise ValidationError('Нельзя подписаться на самого себя.')
+        serializer.save(user=self.request.user)
