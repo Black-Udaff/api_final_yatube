a1dbd83a6ea9ee7e495d73c4421e35b7a8975df9
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-12-21 16:30:25 +0300

    333

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
-
+from rest_framework import routers
+from rest_framework.authtoken import views
+from api.views import PostViewSet, GroupViewSet, CommentViewSet, FolowViewSet
 urlpatterns = [
     path('v1/', include('djoser.urls')),
     # JWT-эндпоинты, для управления JWT-токенами:
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -22,3 +22,35 @@ class FolowViewSet(viewsets.ModelViewSet):
         if serializer.validated_data.get('following') == self.request.user:
             raise ValidationError('Нельзя подписаться на самого себя.')
         serializer.save(user=self.request.user)
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [
+        IsAuthorPermission,
+    ]
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(author=user)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    http_method_names = ['get']
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthorPermission,
+    ]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
